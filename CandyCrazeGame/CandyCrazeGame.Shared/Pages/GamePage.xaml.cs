318c82d2a607baaebbb491f06c29904393bde89e
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-10-26 12:48:22 +0600

    solution renamed

--- CandyCrazeGame/CandyCrazeGame.Shared/Pages/GamePage.xaml.cs
+++ CandyCrazeGame/CandyCrazeGame.Shared/Pages/GamePage.xaml.cs
@@ -4,7 +4,6 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using System;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using Windows.Foundation;
 
